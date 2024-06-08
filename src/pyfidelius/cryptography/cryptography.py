55b57aab305289c056eacc5fb7b3a38708f2364c
Nikhil Raj <nikhil25803@gmail.com>
2024-06-09 01:10:52 +0530

    added decryption method

--- src/pyfidelius/cryptography/cryptography.py
+++ src/pyfidelius/cryptography/cryptography.py
@@ -166,6 +166,64 @@ class Cryptography:
             log.error("Unable to encrypt message.\nError: ", e)
             return None
 
+    def decrypt(
+        self,
+        encrypted_data,
+        sender_nonce,
+        requester_nonce,
+        sender_private_key,
+        requester_public_key,
+    ):
+        """
+        Decrypt the encoded data back to original string.
+
+        Arguments:
+        - `encrypted_data`: The encrypted data which is required to decrypt.
+        - `sender_nonce`: The nonce (number used once) of the sender for encryption.
+        - `requester_nonce`: The nonce of the requester for encryption.
+        - `sender_private_key`: The private key of the sender for encryption.
+        - `requester_public_key`: The public key of the requester for encryption.
+
+        Response:
+        ```json
+        {
+            "decryptedData": "Hello World"
+        }
+        """
+        try:
+            if not all(
+                map(
+                    self.__isBase64,
+                    [
+                        sender_nonce,
+                        requester_nonce,
+                        sender_private_key,
+                        requester_public_key,
+                    ],
+                )
+            ):
+                log.warning(
+                    "sender_nonce, requester_nonce, sender_private_key, and requester_public_key must be base64 encoded"
+                )
+                return None
+            output = self.__execute_cli_commands(
+                [
+                    "d",
+                    encrypted_data,
+                    sender_nonce,
+                    requester_nonce,
+                    sender_private_key,
+                    requester_public_key,
+                ]
+            )
+
+            encrypted_data = self.__parse_output_to_dict(output)
+            return encrypted_data
+
+        except Exception as e:
+            log.error("Unable to decrypt the encrypted data.\nError: ", e)
+            return None
+
 
 ecdh = Cryptography()
 key_materials = ecdh.generate_key_material()
@@ -178,3 +236,11 @@ encodeed_data = ecdh.encrypt(
     requester_private_key="BAheD5rUqTy4V5xR4/6HWmYpopu5CO+KO8BECS0udNqUTSNo91TIqIIy1A4Vh+F94c+n9vAcwXU2bGcfsI5f69Y=",
 )
 print("\nEncrypted data: \n", encodeed_data)
+decoded_data = ecdh.decrypt(
+    encrypted_data="pzMvVZNNVtJzqPkkxcCbBUWgDEBy/mBXIeT2dJWI16ZAQnnXUb9lI+S4k8XK6mgZSKKSRIHkcNvJpllnBg548wUgavBa0vCRRwdL6kY6Yw==",
+    sender_nonce="6uj1RdDUbcpI3lVMZvijkMC8Te20O4Bcyz0SyivX8Eg=",
+    requester_nonce="lmXgblZwotx+DfBgKJF0lZXtAXgBEYr5khh79Zytr2Y=",
+    sender_private_key="DMxHPri8d7IT23KgLk281zZenMfVHSdeamq0RhwlIBk=",
+    requester_public_key="BABVt+mpRLMXiQpIfEq6bj8hlXsdtXIxLsspmMgLNI1SR5mHgDVbjHO2A+U4QlMddGzqyEidzm1AkhtSxSO2Ahg=",
+)
+print("\nDecoded data: \n", decoded_data)
